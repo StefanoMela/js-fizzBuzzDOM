5339180ad18ceb7579bc5f976693882a5e1ac802
StefanoMela <stefano.mela25@gmail.com>
2023-07-21 13:23:26 +0200

    first commit

---
+++ JS/myscript.js
@@ -0,0 +1,26 @@
+// SCRIVI UN PROGRAMMA CHE STAMPI IN CONSOLE I NUMERI DA 1 A 100
+// FAI IN MODO CHE IL PROGRAMMA RICONOSCA I MULTIPLI DI 3, 5 E 3 & 5
+
+
+for (let i = 0; i <= 100; i++) { // CREO CICLO PER STAMPARE IN CONSOLE NUMERI DA 1 A 100
+  //  console.log(i);            // CONSOLE.LOG DI PROVA
+
+    if (i % 3 == 0 && i % 5 == 0) {
+
+        console.log(i + "fizzbuzz");    // CONSOLE.LOG DI PROVA
+
+    } else if (i % 3 == 0) {
+
+        console.log(i + "fizz");    // CONSOLE.LOG DI PROVA
+
+    } else if (i % 5 == 0) {
+
+        console.log(i + "buzz");    // CONSOLE.LOG DI PROVA
+    }
+
+    // const myDiv = document.createElement(div);   // PROVE DI MODIFICA DEL DOM
+    // document.appendChild(myDiv);
+    // myDiv.innerHTML = i;
+
+};
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+## TRACCIA
+
+Codifica un programma che stampi in console i numeri da 1 a 100; in seguito, fai si che il programma riconosca i multipli di 3, 5 e 3 & 5 e che stampi una determinata parola per ognuno di essi.
