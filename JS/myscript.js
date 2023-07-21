09e110aad40d5e16682e22938e9f95a92f8ca8e6
StefanoMela <stefano.mela25@gmail.com>
2023-07-21 15:16:27 +0200

    fix starting number

--- JS/myscript.js
+++ JS/myscript.js
@@ -2,7 +2,7 @@
 // FAI IN MODO CHE IL PROGRAMMA RICONOSCA I MULTIPLI DI 3, 5 E 3 & 5
 
 
-for (let i = 0; i <= 100; i++) { // CREO CICLO PER STAMPARE IN CONSOLE NUMERI DA 1 A 100
+for (let i = 1; i <= 100; i++) { // CREO CICLO PER STAMPARE IN CONSOLE NUMERI DA 1 A 100
     //  console.log(i);            // CONSOLE.LOG DI PROVA
     
     if (i % 3 == 0 && i % 5 == 0) {
@@ -18,13 +18,15 @@ for (let i = 0; i <= 100; i++) { // CREO CICLO PER STAMPARE IN CONSOLE NUMERI DA
         console.log(i + "buzz");    // CONSOLE.LOG DI PROVA
     }
     
+    const myDiv = document.createElement(div);   // PROVE DI MODIFICA DEL DOM
+    document.appendChild(myDiv);
+    myDiv.innerHTML = i;        
 };
+
 
 // const myDiv = document.getElementsByClassName(".box-target");
 // function fizzBlock () {
 //     const newDiv = document.createElement("div");
 //     myDiv.appendChild(newDiv);
 // }
-// const myDiv = document.createElement(div);   // PROVE DI MODIFICA DEL DOM
-// document.appendChild(myDiv);
-// myDiv.innerHTML = i;
+
