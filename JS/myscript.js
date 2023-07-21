0e1052ab71c35aa3ead725d2e89d2675546f1a51
StefanoMela <stefano.mela25@gmail.com>
2023-07-21 16:34:17 +0200

    feat: add colors and box schemes

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,43 +1,33 @@
 // SCRIVI UN PROGRAMMA CHE STAMPI IN CONSOLE I NUMERI DA 1 A 100
 // FAI IN MODO CHE IL PROGRAMMA RICONOSCA I MULTIPLI DI 3, 5 E 3 & 5
 
-    const newDiv = document.getElementById("box-target");
+const newDiv = document.getElementById("box-target");
 
-for (let i = 1; i <= 100; i++) { // CREO CICLO PER STAMPARE IN CONSOLE NUMERI DA 1 A 100
-    //  console.log(i);            // CONSOLE.LOG DI PROVA
-    
+for (let i = 1; i <= 100; i++) {        // CREO CICLO PER STAMPARE IN CONSOLE NUMERI DA 1 A 100
+
     let output = i;
-    
-    if (i % 3 == 0 && i % 5 == 0) {
-        
-        // output = "fizzbuzz";    // CONSOLE.LOG DI PROVA
-        const myDiv = document.createElement("div");
-        myDiv.innerHTML = output; 
-        myDiv.classList.add("fizzBuzz");
-        newDiv.append(myDiv);
-
-
-        
-    } else if (i % 3 == 0) {
-        
-        output = "fizz";    // CONSOLE.LOG DI PROVA
-        const myDiv = document.createElement("div");
-        myDiv.innerHTML = output; 
-        myDiv.classList.add("fizz");
-        newDiv.append(myDiv);
-        
-    } else if (i % 5 == 0) {
-        
-        output = "buzz";    // CONSOLE.LOG DI PROVA
-        const myDiv = document.createElement("div");
-        myDiv.innerHTML = output; 
-        myDiv.classList.add("buzz");
-        newDiv.append(myDiv);
-    } 
-    output = output;
+    let myClass = "number";
     const myDiv = document.createElement("div");
-    myDiv.innerHTML = output; 
-    myDiv.classList.add("number");
+
+    if (i % 3 == 0 && i % 5 == 0) {
+
+        myClass = "fizzbuzz";
+        output = "FizzBuzz";
+
+
+    } else if (i % 3 == 0) {
+
+        myClass = "fizz";
+        output = "Fizz";
+
+    } else if (i % 5 == 0) {
+
+        myClass = "buzz";
+        output = "Buzz";
+    }
+
+    myDiv.innerHTML = output;
+    myDiv.classList.add(myClass);
     newDiv.append(myDiv);
-    
+
 };
