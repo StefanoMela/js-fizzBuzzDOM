b34ec86b72f0e1340fb665606109a040e4ff8f7b
StefanoMela <stefano.mela25@gmail.com>
2023-07-21 16:00:21 +0200

    feat: stamp on screen w/ some style

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,35 +1,43 @@
 // SCRIVI UN PROGRAMMA CHE STAMPI IN CONSOLE I NUMERI DA 1 A 100
 // FAI IN MODO CHE IL PROGRAMMA RICONOSCA I MULTIPLI DI 3, 5 E 3 & 5
 
+    const newDiv = document.getElementById("box-target");
 
 for (let i = 1; i <= 100; i++) { // CREO CICLO PER STAMPARE IN CONSOLE NUMERI DA 1 A 100
     //  console.log(i);            // CONSOLE.LOG DI PROVA
     
+    let output = i;
+    
     if (i % 3 == 0 && i % 5 == 0) {
         
-        console.log(i + "fizzbuzz");    // CONSOLE.LOG DI PROVA
+        // output = "fizzbuzz";    // CONSOLE.LOG DI PROVA
+        const myDiv = document.createElement("div");
+        myDiv.innerHTML = output; 
+        myDiv.classList.add("fizzBuzz");
+        newDiv.append(myDiv);
+
+
         
     } else if (i % 3 == 0) {
         
-        console.log(i + "fizz");    // CONSOLE.LOG DI PROVA
+        output = "fizz";    // CONSOLE.LOG DI PROVA
+        const myDiv = document.createElement("div");
+        myDiv.innerHTML = output; 
+        myDiv.classList.add("fizz");
+        newDiv.append(myDiv);
         
     } else if (i % 5 == 0) {
         
-        console.log(i + "buzz");    // CONSOLE.LOG DI PROVA
-    } else {
-
-        console.log(i);
-    }
+        output = "buzz";    // CONSOLE.LOG DI PROVA
+        const myDiv = document.createElement("div");
+        myDiv.innerHTML = output; 
+        myDiv.classList.add("buzz");
+        newDiv.append(myDiv);
+    } 
+    output = output;
+    const myDiv = document.createElement("div");
+    myDiv.innerHTML = output; 
+    myDiv.classList.add("number");
+    newDiv.append(myDiv);
     
-    const myDiv = document.createElement(div);   // PROVE DI MODIFICA DEL DOM
-    document.appendChild(myDiv);
-    myDiv.innerHTML = i;        
 };
-
-
-// const myDiv = document.getElementsByClassName(".box-target");
-// function fizzBlock () {
-//     const newDiv = document.createElement("div");
-//     myDiv.appendChild(newDiv);
-// }
-
