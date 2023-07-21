281822b568907a3c98093c6f52c5c37a1d8741df
StefanoMela <stefano.mela25@gmail.com>
2023-07-21 14:03:17 +0200

    feat: some more try to print

--- JS/myscript.js
+++ JS/myscript.js
@@ -3,24 +3,28 @@
 
 
 for (let i = 0; i <= 100; i++) { // CREO CICLO PER STAMPARE IN CONSOLE NUMERI DA 1 A 100
-  //  console.log(i);            // CONSOLE.LOG DI PROVA
-
+    //  console.log(i);            // CONSOLE.LOG DI PROVA
+    
     if (i % 3 == 0 && i % 5 == 0) {
-
+        
         console.log(i + "fizzbuzz");    // CONSOLE.LOG DI PROVA
-
+        
     } else if (i % 3 == 0) {
-
+        
         console.log(i + "fizz");    // CONSOLE.LOG DI PROVA
-
+        
     } else if (i % 5 == 0) {
-
+        
         console.log(i + "buzz");    // CONSOLE.LOG DI PROVA
     }
-
-    // const myDiv = document.createElement(div);   // PROVE DI MODIFICA DEL DOM
-    // document.appendChild(myDiv);
-    // myDiv.innerHTML = i;
-
+    
 };
 
+// const myDiv = document.getElementsByClassName(".box-target");
+// function fizzBlock () {
+//     const newDiv = document.createElement("div");
+//     myDiv.appendChild(newDiv);
+// }
+// const myDiv = document.createElement(div);   // PROVE DI MODIFICA DEL DOM
+// document.appendChild(myDiv);
+// myDiv.innerHTML = i;
